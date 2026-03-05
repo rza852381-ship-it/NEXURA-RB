29adf67a642c37d9824b51b2fea2101dd3492ab7
rza852381-ship-it <rza852381@gmail.com>
2026-03-06 00:12:41 +0300

    Add Social component with social media buttons

---
+++ client/src/Social.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Social() {
+  return (
+    <div>
+      <h1>ربط وسائل التواصل</h1>
+
+      <button>ربط فيسبوك</button>
+      <button>ربط انستغرام</button>
+      <button>ربط تيك توك</button>
+
+    </div>
+  );
+}

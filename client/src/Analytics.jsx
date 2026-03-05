448154a172c25338e46ade60eab79148292f34af
rza852381-ship-it <rza852381@gmail.com>
2026-03-06 00:12:11 +0300

    Add Analytics component with Arabic content

---
+++ client/src/Analytics.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Analytics() {
+  return (
+    <div>
+      <h1>لوحة التحليلات</h1>
+      <p>هنا ستظهر تحليلات المبيعات والحملات</p>
+    </div>
+  );
+}

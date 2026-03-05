e8b2c9a41d9b519d52a8495a2b31bcfe0a9ccf91
rza852381-ship-it <rza852381@gmail.com>
2026-03-06 00:16:31 +0300

    Add Analytics component with initial content

---
+++ client/src/pages/Analytics.tsx
@@ -0,0 +1,10 @@
+ import React from "react";
+
+export default function Analytics() {
+  return (
+    <div>
+      <h1>Analytics</h1>
+      <p>تحليلات الحملات والمبيعات ستظهر هنا</p>
+    </div>
+  );
+}
